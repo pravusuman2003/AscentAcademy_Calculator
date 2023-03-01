d2ac2d26bdbe2f7dc54be20da5fd54fc1372c287
pravusuman2003 <102952581+pravusuman2003@users.noreply.github.com>
2023-03-01 13:49:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let result = document.getElementById('result');
+
+function getResult(num) {
+  result.value += num;
+}
+
+function clearResult() {
+  result.value = '';
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch (e) {
+    alert('Invalid Input!');
+    clearResult();
+  }
+}
